ff2d2da8aa9bd3f9915835d1e5b0cad256302d31
Richard Hsu <45455105+bung8833@users.noreply.github.com>
2024-05-14 22:00:56 +0800

    added AuthService class for user register & login

---
+++ Services/AuthService.cs
@@ -0,0 +1,30 @@
+﻿
+namespace dotnet_rpg.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
---
+++ Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_rpg.Services
+{
+    public interface IAuthService
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
